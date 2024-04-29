e2a5e54b0552c067235bf50062639e3f2ecc1aa2
YEL!ne <1638317920@qq.com>
2024-04-29 17:07:31 +0800

    FEAT : loading page

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,51 @@
-<script setup>
-import navigation from './components/navigation/navigation.vue'
-</script>
-
 <template>
-   <navigation />
-   <router-view></router-view>
+   <div v-if="isReady">
+      <navigation />
+      <router-view></router-view>
+   </div>
+   <div>
+      <loading :onLoading="onLoading" ref="childrenRef" />
+   </div>
 </template>
+
+<script setup lang="ts">
+import { ref, onMounted, onBeforeMount, Ref } from 'vue'
+import loading from './components/index/loading.vue'
+import navigation from './components/navigation/navigation.vue'
+
+let interval
+
+const isReady: Ref<boolean> = ref(false)
+let onLoading: Ref<number> = ref(0)
+const childrenRef: Ref<any> = ref(null)
+
+const finishLoading = function () {
+   childrenRef.value.finishLoading()
+}
+
+onBeforeMount(() => {
+   interval = setInterval(
+      () => {
+         onLoading.value += Math.floor(Math.random() * 50) 
+         if (onLoading.value >= 100) {
+            clearInterval(interval)
+            isReady.value = true
+         }
+      },
+      500 + Math.random() * 500
+   ) 
+})
+
+onMounted(() => {
+   setTimeout(() => {
+      clearInterval(interval)
+      onLoading.value = 100
+   }, 2000)
+   setTimeout(() => {
+      finishLoading()
+   }, 3200)
+   setTimeout(() => {
+      isReady.value = true
+   }, 3500)
+})
+</script>
---
+++ src/components/index/loading.vue
@@ -0,0 +1,45 @@
+<template>
+   <div class="absolute w-full h-[100vh] bg-black z-50 flex loadingPage">
+      <div class="w-[30vw] m-auto relative">
+         <img src="@/assets/img/circle_white.webp" alt="" class="circle" />
+         <div
+            class="text-4xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-slate-200"
+            style="font-family: 'Times New Roman', Times, serif;"
+         >
+            {{ onLoading }}
+         </div>
+      </div>
+   </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, onMounted } from 'vue'
+import gsap from 'gsap'
+
+const finishLoading = () => {
+   gsap.to('.loadingPage', {
+      opacity: 0,
+      duration: 0.5,
+      onComplete: () => {
+         document.querySelector('.loadingPage')?.remove()
+      }
+   })
+}
+
+defineProps({
+   onLoading: Number
+})
+
+defineExpose({
+   finishLoading
+})
+
+onMounted(() => {
+   gsap.to('.circle', {
+      rotation: 360,
+      repeat: -1,
+      ease: 'linear',
+      duration: 10 // 这里可以设置旋转一圈所需的时间，单位是秒
+   })
+})
+</script>
